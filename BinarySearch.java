69e267457673ad30af6bc94b763dfba9018c65e9
Menaga-J-711 <jmenaga7112004@gmail.com>
2025-06-02 00:20:47 +0530

    Assingment_Day_8

---
+++ BinarySearch.java
@@ -0,0 +1,70 @@
+// public class BinarySearch
+//  {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//     }
+// }
+///leet code 1095
+/// /**
+//  * // This is MountainArray's API interface.
+//  * // You should not implement it, or speculate about its implementation
+//  * interface MountainArray {
+//  *     public int get(int index) {}
+//  *     public int length() {}
+//  * }
+//  */
+ 
+// class Solution {
+//     public int findInMountainArray(int target, MountainArray mountainArr) {
+//         int n= mountainArr.length();
+//         int start=0;
+//         int end= n-1;
+//         while(start<end){
+//             int mid= start+ (end-start)/2;
+//             int mid_val= mountainArr.get(mid);
+//             int nextval= mountainArr.get(mid+1);
+//             if(mid_val<nextval){
+//                 start= mid+1;
+//             }
+//             else{
+//                 end=mid;
+//             }
+
+//             }
+//             int peak = start;
+//             int result= binarysearch (mountainArr,0,peak,target,true);
+//             if(result != -1){
+//             return result;
+//             }
+//             return binarysearch(mountainArr,peak+1,n-1, target,false);
+        
+//     }
+//     public int binarysearch(MountainArray arr,int start,int end,int target,boolean ascending){
+//         while(start<=end){
+//             int mid=start+(end-start)/2;
+//             int midval= arr.get(mid);
+//             if(midval == target){
+//                 return mid;
+//             }
+//             if(ascending){
+//                 if(target<midval){
+//                     end= mid-1;
+
+//                 }
+//                 else{
+//                     start= mid+1;
+//                 }
+//             }
+//             else{
+//                 if(target>midval){
+//                     end= mid-1;
+
+//                 }
+//                 else{
+//                     start=mid+1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// }
